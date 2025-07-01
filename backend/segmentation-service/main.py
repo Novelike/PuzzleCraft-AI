60bb9b72dff04c175e6368a9d84e4a4765579db4
kjh <kimjeahyun77@gmail.com>
2025-07-01 15:57:37 +0900

    Modified port

--- backend/segmentation-service/main.py
+++ backend/segmentation-service/main.py
@@ -303,7 +303,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=8002,
+        port=8006,
         reload=True,
         log_level="info"
     )
